5af045502ce90b3fd16485f4281a98c4c88014bd
Alastor64 <915789546@qq.com>
2025-12-18 00:07:46 +0800

    create Base the basic class of Book and User

---
+++ include/Base.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "IO.hpp"
+class Base
+{
+protected:
+    virtual IO_base *Data() = 0; // WARNING nullptr runtime error!
+
+public:
+    int THIS;
+    void write() // 将自己插入文件
+    {
+        THIS = Data()->Peep();
+        Data()->Write(this);
+    }
+    void updata() // 用自己更新文件
+    {
+        Data()->Update(this, THIS);
+    }
+    void read() // 从文件读取自己
+    {
+        Data()->Read(this, THIS);
+    }
+};
