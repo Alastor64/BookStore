66d44e0e761c97284cc8ed53fae64049712d8a32
Alastor64 <915789546@qq.com>
2025-12-18 00:15:19 +0800

    create Vector

---
+++ include/Vector.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "IO.hpp"
+#include <vector>
+#include <string>
+using std::string;
+template <class T>
+class Vector : public std::vector<T>
+{
+protected:
+    const string filename;
+    IO<T, 1> *Data() // return IO<T, 1>::instance(filename);
+    {
+        return IO<T, 1>::instance(filename);
+    }
+
+public:
+    Vector(const string &FN) : filename(FN) // reload and then initialize if failed 未测试
+    {
+        this->clear();
+        int n = Data()->Get_info(0);
+        if (n)
+        {
+            T tmp;
+            for (int i = 0; i < n; i++)
+            {
+                Data()->Read(&tmp, Data()->frontIndex() + i * sizeof(T));
+                this->push_back(tmp);
+            }
+        }
+    }
+
+    void update() // WARNING time comsumption O(size) !!! 未测试
+    {
+        Data()->initialise();
+        Data()->Write_info(this->size(), 0);
+        for (int i = 0; i < this->size(); i++)
+        {
+            Data()->Write(&this->at(i));
+        }
+    }
+};
